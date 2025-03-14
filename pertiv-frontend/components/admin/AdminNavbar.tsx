b70f862d74ec497e547d06c9bb17928a561026b4
dena <devdenn7@gmail.com>
2025-03-14 15:27:22 +0700

    feat: add logout handler

--- pertiv-frontend/components/admin/AdminNavbar.tsx
+++ pertiv-frontend/components/admin/AdminNavbar.tsx
@@ -1,4 +1,23 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { deleteCookie } from '@/lib/actions/auth.action';
+
 const AdminNavbar = () => {
+  const router = useRouter();
+  const logoutHandler = async () => {
+    await deleteCookie();
+    console.log('before redirect');
+    router.push('/login');
+
+    console.log('after redirect');
+  };
   return (
     <header>
       <nav className="bg-blue-700">
@@ -7,13 +26,18 @@ const AdminNavbar = () => {
             <div className="flex space-x-4">
               <p className="text-white">Hi, Admin</p>
             </div>
-            <div>
-              <div className="relative ml-3">
+            <DropdownMenu>
+              <DropdownMenuTrigger>
                 <div className="rounded-full">
                   <div className="size-8 rounded-full bg-red-500"></div>
                 </div>
-              </div>
-            </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem className="cursor-pointer">
+                  <div onClick={() => logoutHandler()}>Logout</div>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
       </nav>
